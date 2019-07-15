164b59b2e5ce3f664687519c4fff8a22d91c70fc
adesdev <lobotijo@gmail.com>
2019-07-15 16:30:33 +0700

    addons custom Module in Odoo

---
+++ mukidi_theme/__manifest__.py
@@ -0,0 +1,27 @@
+{
+  'name': 'Mukidi Theme',
+  'author': 'ades',
+  'version': '0.1',
+  'depends': [
+      'website',
+      'website_blog', 
+      'sale'
+    # 'nama_modul',
+  ],
+  'data': [
+     'views/layout.xml',
+     'views/pages.xml',
+     'views/assets.xml'
+  ],
+  'qweb': [
+    # 'static/src/xml/nama_widget.xml',
+  ],
+  'sequence': 1,
+  'auto_install': False,
+  'installable': True,
+  'application': True,
+  'category': 'Theme/Creative',
+  'summary': 'Custom Your Company Design',
+  'website': 'https://www.nineone.com/',
+  'description': '-'
+}
